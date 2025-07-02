1ccee1bbaf67797a27a89507fecb735f03368549
zuhairaz <zuhairaz@heitech.com.my>
2025-07-02 10:44:52 +0800

    added static.js to flexibly get all public items

---
+++ api/static.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = async (req, res) => {
+    // Get the requested file path from the URL
+    const filePath = path.join(__dirname, '../public', req.query.file);
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.status(404).send('File not found.');
+            return;
+        }
+
+        // Basic content type detection
+        let contentType = 'text/plain';
+        if (filePath.endsWith('.html')) contentType = 'text/html';
+        else if (filePath.endsWith('.css')) contentType = 'text/css';
+        else if (filePath.endsWith('.js')) contentType = 'application/javascript';
+        else if (filePath.endsWith('.png')) contentType = 'image/png';
+        else if (filePath.endsWith('.jpg') || filePath.endsWith('.jpeg')) contentType = 'image/jpeg';
+
+        res.setHeader('Content-Type', contentType);
+        res.status(200).send(data);
+    });
+};
