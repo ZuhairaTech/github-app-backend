1937f89806d2a72159bc271c5b4a5e29654783cf
zuhairaz <zuhairaz@heitech.com.my>
2025-07-02 10:48:02 +0800

    try CORS

--- api/static.js
+++ api/static.js
@@ -2,6 +2,10 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     // Get the requested file path from the URL
     const filePath = path.join(__dirname, '../public', req.query.file);
 
